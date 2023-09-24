8cd699f765470c8c8f555a80a688c54c07de5a14
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-09-24 15:02:56 +0300

    Optimized version using useQUery, error handling, loader

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,35 @@
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import fetchPet from './fetchPet';
 
 const Details = () => {
     const { id } = useParams();
-    return <h2>{id}</h2>;
+    const results = useQuery(['details', id], fetchPet); //details is the name of the cache, id is the parameter that is used in the fetchPet function, the fetchPet function is the function that is called to get the data if it is not in the cache
+
+    if (results.isError) return <h1>Error: {results.error.message}</h1>;
+
+    if (results.isLoading) {
+        return (
+            <div className="loading-pane">
+                <h2 className="loader">@</h2>
+            </div>
+        );
+    }
+
+    const pet = results.data.pets[0];
+
+    return (
+        <div className="details">
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>
+                    {pet.animal} - {pet.breed} - {pet.city} - {pet.state}{' '}
+                </h2>
+                <button>Adopt {pet.name}</button>
+                <p>{pet.description}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Details;
