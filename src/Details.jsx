8aac923fa5324fdf6cd8354de7f39ec28e935b37
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-09-25 13:54:26 +0300

    Implementing Modals with Portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import ErrorBoundary from './ErrorBoundary';
 import Carousel from './Carousel';
 import fetchPet from './fetchPet';
+import Modal from './Modal';
 
 const Details = () => {
+    const [showModal, setShowModal] = useState(false);
     const { id } = useParams();
     const results = useQuery(['details', id], fetchPet); //details is the name of the cache, id is the parameter that is used in the fetchPet function, the fetchPet function is the function that is called to get the data if it is not in the cache
 
@@ -28,8 +31,26 @@ const Details = () => {
                 <h1>{pet.name}</h1>
                 <h2>
                     {pet.animal} - {pet.breed} - {pet.city} - {pet.state}{' '}
-                    <button>Adopt {pet.name}</button>
+                    <button onClick={() => setShowModal(true)}>
+                        {' '}
+                        Adopt {pet.name}
+                    </button>
                     <p>{pet.description}</p>
+                    {showModal ? (
+                        <Modal>
+                            <div>
+                                <h1>Would you like to adopt {pet.name}?</h1>
+                                <div className="buttons">
+                                    <button onClick={() => setShowModal(false)}>
+                                        Yes
+                                    </button>
+                                    <button onClick={() => setShowModal(false)}>
+                                        No
+                                    </button>
+                                </div>
+                            </div>
+                        </Modal>
+                    ) : null}
                 </h2>
             </div>
         </div>
